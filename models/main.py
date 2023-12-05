403bce6b322291270cda8c8295338c2667294af6
Vilaca84 <phillipe.vilaca@gmail.com>
2023-12-05 12:03:48 +0100

    initial modeling for gurobipy saving mps

--- models/main.py
+++ models/main.py
@@ -1,14 +1,22 @@
 import gurobipy as gb
 import pandas as pd
 import scipy as sp
+import os
 
 from Interpretable_Optimization.models.utils_models.utils_modeling import create_original_model
 
+# Get the directory of the current script (main.py)
+current_script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Example usage:
+# Navigate up one levels to get to the root of the project
+project_root = os.path.dirname(current_script_dir)
+
+data_path = os.path.join(project_root, 'data')
+model_filename = os.path.join(data_path, "original_model.mps")
+
+# creating the original_model
 n_variables = 3  # Number of decision variables
 n_constraints = 2  # Number of constraints
-
 original_model = create_original_model(n_variables, n_constraints)
 if original_model:
     original_model.optimize()
@@ -17,4 +25,13 @@ if original_model:
     for var in original_model.getVars():
         print(f"{var.VarName} =", var.x)
 
+# saving the original model
+original_model.write(model_filename)
+
+# creating the presolved model
+presolved_model = original_model.presolve()
+
+# Access constraint matrix A and right-hand side vector b of the original model
+A = original_model.getA()
+b = original_model.getAttr('RHS', original_model.getConstrs())
 
