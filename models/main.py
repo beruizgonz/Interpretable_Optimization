abee773809afe9cb2bef546457a5ba78396f4501
Vilaca84 <phillipe.vilaca@gmail.com>
2023-12-06 00:46:23 +0100

    logs

--- models/main.py
+++ models/main.py
@@ -1,17 +1,20 @@
 import os
 import gurobipy as gp
-import scipy as sp
-import pandas as pd
+import logging
+from datetime import datetime
 from Interpretable_Optimization.models.utils_models.utils_modeling import create_original_model, get_model_matrices, \
     save_json, build_model_from_json
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel('INFO')
 
 if __name__ == "__main__":
 
     # parameters to run
-    config = {"create_model": {"val": False,
-                               "n_variables": 3,
-                               "n_constraints": 2},
-              "load_model": {"val": True,
+    config = {"create_model": {"val": True,
+                               "n_variables": 10,
+                               "n_constraints": 25},
+              "load_model": {"val": False,
                              "name": 'original_model.mps'},
               "print_model_sol": True,
               "save_original_model": True,
@@ -29,10 +32,16 @@ if __name__ == "__main__":
     data_path = os.path.join(project_root, 'data')
 
     # creating the original_model
+    log.info(
+        f"{str(datetime.now())}: Creating the original_model with {config['create_model']['n_variables']} variables"
+        f"and {config['create_model']['n_constraints']} constraints...")
+
     if config['create_model']['val']:
         original_model = create_original_model(config['create_model']['n_variables'],
                                                config['create_model']['n_constraints'])
     else:
+        log.info(
+            f"{str(datetime.now())}: Loading the original_model...")
         # Specify the path to the MPS file you want to load
         model_to_load = os.path.join(data_path, config['load_model']['name'])
 
@@ -40,35 +49,67 @@ if __name__ == "__main__":
         original_model = gp.read(model_to_load)
 
     # solving the original model
+    log.info(
+        f"{str(datetime.now())}: Solving the original_model...")
     original_model.optimize()
     initial_sol = original_model.objVal
 
-    # printing the original model solution
-    if config['print_model_sol']:
-        print("Original Model:")
-        print("Optimal Objective Value =", original_model.objVal)
-        for var in original_model.getVars():
-            print(f"{var.VarName} =", var.x)
-
     # saving the original model
     if config["save_original_model"]:
+        log.info(
+            f"{str(datetime.now())}: Saving the original_model...")
         model_to_save = os.path.join(data_path, "original_model.mps")
         original_model.write(model_to_save)
 
     # Access constraint matrix A and right-hand side (RHS) vector b of the original model
+    log.info(
+        f"{str(datetime.now())}: Accessing matrix A, right-hand side b, cost function c, and the bounds of "
+        f"the original model...")
     A, b, c, lb, ub = get_model_matrices(original_model)
 
     # saving the matrix
     if config['save_matrices']:
+        log.info(
+            f"{str(datetime.now())}: Saving A, b, c, lb and ub...")
         save_json(A, b, c, lb, ub, data_path)
 
     # creating models from json files
+    log.info(
+        f"{str(datetime.now())}: Creating model from  A, b, c, lb and ub...")
     created_model = build_model_from_json(data_path)
 
     # solving created model
+    log.info(
+        f"{str(datetime.now())}: Solving the created_model...")
     created_model.optimize()
     final_sol = created_model.objVal
+
+    # printing the original model solution
+    if config['print_model_sol']:
+        print("============ Original Model ============")
+        print("Optimal Objective Value =", original_model.objVal)
+        for var in original_model.getVars():
+            print(f"{var.VarName} =", var.x)
+
+        print("============ Created Model ============")
+        print("Optimal Objective Value =", original_model.objVal)
+        for var in original_model.getVars():
+            print(f"{var.VarName} =", var.x)
 
     # creating the presolved model
     if config['create_presolved']:
         presolved_model = original_model.presolve()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
